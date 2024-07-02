b164f20443267c62605d8bfdac50384e52a91553
Siddhi Mayekar <117754791+siddhismayekar@users.noreply.github.com>
2024-07-02 22:42:22 +0530

    Update

--- main/api.py
+++ main/api.py
@@ -1,9 +1,16 @@
 #api.py
-import os
-from dotenv import load_dotenv
-load_dotenv()
+# import os
+# from dotenv import load_dotenv
+# load_dotenv()
 
-api_key = os.getenv("api_key")
-access_token=os.getenv("access_token")
-WebAPIKey =os.getenv("WebAPIKey")
-email=os.getenv("email")
+# api_key = os.getenv("api_key")
+# access_token=os.getenv("access_token")
+# WebAPIKey =os.getenv("WebAPIKey")
+# email=os.getenv("email")
+
+import streamlit as st
+# Access secrets directly from Streamlit's secrets management
+api_key = st.secrets["api_key"]
+access_token = st.secrets["access_token"]
+WebAPIKey = st.secrets["WebAPIKey"]
+email = st.secrets["email"]
