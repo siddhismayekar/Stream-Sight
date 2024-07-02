bed4a29f2dc0486426690bf5460d1648bb064e51
Siddhi Mayekar <117754791+siddhismayekar@users.noreply.github.com>
2024-07-02 23:54:09 +0530

    Update

--- main/pg/home.py
+++ main/pg/home.py
@@ -4,10 +4,6 @@ from streamlit_lottie import st_lottie
 import json
 import base64
 import api
-# Function to load Lottie animations from a file
-def load_lottie_file(filepath: str):
-    with open(filepath, "r") as f:
-        return json.load(f)
 
 # Function to convert image to base64
 def get_base64_image(image_path):
@@ -17,9 +13,9 @@ def get_base64_image(image_path):
 def app():
     # Load Lottie animations
     lottie_hero = "animation/man_watching_movie.json"
-    logo_net = load_lottie_file("animation/netflix_logo.json")
-    logo_pri = load_lottie_file("animation/prime_video_logo.json")
-    logo_dis = load_lottie_file("animation/disney_logo.json")
+    logo_net = "animation/netflix_logo.json"
+    logo_pri = "animation/prime_video_logo.json"
+    logo_dis = "animation/disney_logo.json"
 
     # Display logos
     l1, l2, l3 = st.columns(3)
